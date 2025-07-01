f9df0bb8a35d4cad777985389a688dead24e067e
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-01 22:10:41 +0545

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,47 @@
 import React, {useState, useEffect} from 'react'
 import './App.css';
+import axios from 'axios';
+
+
+
 const App = () => {
+
   const [items, setItems] = useState([]);
-  const [dataIsLoaded, setDataIsLoaded] = useState (false);
+  const [dataIsLoaded, setDataIsLoaded] = useState(false);
 
   useEffect(() =>{
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res) => res.json())
-    .then((json) =>{
-      setItems(json);
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then((res) =>{
+      setItems(res.data);
       setDataIsLoaded(true);
-
     });
-  }, []);
+  },[]);
 
-  if (!dataIsLoaded) {
+  if(!dataIsLoaded){
     return(
       <div>
-        <h1>Please wait some times ....</h1>
+        <h1>Please wait some time .....</h1>
       </div>
     );
   }
   return (
-    <div className='App'>
-      <h1 className='geeks'>GeeksForGeeks</h1>
+    <div className="App">
+      <h1 className='geeks'>GeeksforGeeks</h1>
       <h3>Fetch data from an API in React</h3>
       <div className="container">
-        {items.map((item) =>(
+        {items.map((item) =>{
           <div className="item" key={item.id}>
             <ol>
               <div>
-                <strong>User_Name: </strong>
-                {items.username},
+                <strong>User_Name :</strong>
+                {item.username},
               </div>
-              <div>Full_Name : {item.name}</div>
-              <div>User_Email : {item.email}</div>
-            </ol>
+              <div>Full_Name: {item.name}</div>
 
+              <div>User_Email: {item.email}</div>
+            </ol>
           </div>
-        ))}
+        })}
       </div>
       
     </div>
