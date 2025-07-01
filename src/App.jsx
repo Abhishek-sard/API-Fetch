5465e577cbebb0051d9ea556321eaf3cfbd44b35
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-01 21:48:00 +0545

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState, useEffect} from 'react'
+import './App.css';
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [dataIsLoaded, setDataIsLoaded] = useState (false);
 
-function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() =>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((json) =>{
+      setItems(json);
+      setDataIsLoaded(true);
 
-  return (
-    <>
+    });
+  }, []);
+
+  if (!dataIsLoaded) {
+    return(
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Please wait some times ....</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    );
+  }
+  return (
+    <div className='App'>
+      <h1 className='geeks'>GeeksForGeeks</h1>
+      <h3>Fetch data from an API in React</h3>
+      <div className="container">
+        {items.map((item) =>(
+          <div className="item" key={item.id}>
+            <ol>
+              <div>
+                <strong>User_Name: </strong>
+                {items.username},
+              </div>
+              <div>Full_Name : {item.name}</div>
+              <div>User_Email : {item.email}</div>
+            </ol>
+
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+    </div>
   )
 }
 
