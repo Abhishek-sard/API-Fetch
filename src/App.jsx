9d2d6dcbef0ff278a14927aab8bb80037c6edc44
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-07-02 08:03:16 +0545

    push

--- src/App.jsx
+++ src/App.jsx
@@ -71,37 +71,37 @@
 
 //using the react query library
 
-import React from 'react';
-import { useQuery } from 'react-query';
+// import React from 'react';
+// import { useQuery } from 'react-query';
 
-// Function to fetch data
-const fetchUsers = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    if (!res.ok) throw new Error('Network error');
-    return res.json();
-};
+// // Function to fetch data
+// const fetchUsers = async () => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users');
+//     if (!res.ok) throw new Error('Network error');
+//     return res.json();
+// };
 
-const App = () => {
-    // Use useQuery to fetch data
-    const { data, error, isLoading } = useQuery('users', fetchUsers);
+// const App = () => {
+//     // Use useQuery to fetch data
+//     const { data, error, isLoading } = useQuery('users', fetchUsers);
 
-    if (isLoading) return <p>Loading...</p>;
-    if (error) return <p>Error loading data</p>;
+//     if (isLoading) return <p>Loading...</p>;
+//     if (error) return <p>Error loading data</p>;
 
-    return (
-        <div>
-            <h1>User List</h1>
-            <ul>
-                {data.map((user) => (
-                    <li key={user.id}>
-                        <h3>{user.username}</h3>
-                        <p>{user.name}</p>
-                        <p>{user.email}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             <h1>User List</h1>
+//             <ul>
+//                 {data.map((user) => (
+//                     <li key={user.id}>
+//                         <h3>{user.username}</h3>
+//                         <p>{user.name}</p>
+//                         <p>{user.email}</p>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// };
 
-export default App;
+// export default App;
